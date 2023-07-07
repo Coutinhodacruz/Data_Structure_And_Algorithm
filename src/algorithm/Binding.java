c6f96f74321bd984a1b848e47cfcbca3b4a56957
onukwufor dominic <dominicrotimi@gmail.com>
2023-07-07 12:01:24 -0400

    updated

---
+++ src/algorithm/Binding.java
@@ -0,0 +1,69 @@
+package algorithm;
+
+public class Binding {
+    public static void main(String[] args) {
+        System.out.println("Before swap");
+        int a = 2;
+        int b = 3;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        System.out.println("After Swap");
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        System.out.println("Before swap");
+//        int a = 2;
+//        int b = 3;
+//        System.out.println("a = " + a);
+//        System.out.println("b = " + b);
+//
+//        System.out.println("After Swap");
+//
+//
+//        a = a ^ b;
+//        b = b ^ a;
+//        a = b ^ a;
+//        b = a ^ b;
+//        b = a ^ b;
+//
+//        System.out.println("a = " + a);
+//        System.out.println("b = " + b);
+
+
+//
+//        System.out.println("Before swap");
+//        int a = 2;
+//        int b = 3;
+//        System.out.println("a = " + a);
+//        System.out.println("b = " + b);
+//
+//        System.out.println("After swap");
+//
+//        a = a * b;
+//        b = a / b;
+//        a = a / b;
+//
+//        System.out.println("a = " + a);
+//        System.out.println("b = " + b);
+//
+//
+//
+    }
+}
